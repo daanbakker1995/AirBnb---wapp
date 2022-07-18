f221e100e6c91e3b78dcd8af41f76dbc7b32114e
DaanBakker <daan.bakker@infocaster.net>
2022-07-18 17:30:42 +0200

    Add project files.

---
+++ AirBnb/Data/AirBnbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AirBnb.Models;
+
+namespace AirBnb.Data
+{
+    public class AirBnbContext : DbContext
+    {
+        public AirBnbContext (DbContextOptions<AirBnbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
